047abb6930852340e8a8f6f55a7b961359564fb5
Marvin Schürz <minetoblend@gmail.com>
2023-08-22 17:32:09 +0200

    Add fields to `EditorRoomUser`

--- osu.Game/Online/Editor/EditorRoomUser.cs
+++ osu.Game/Online/Editor/EditorRoomUser.cs
@@ -1,10 +1,47 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
+using System.Text.Json.Serialization;
+using MessagePack;
+using osu.Game.Online.API.Requests.Responses;
+
 namespace osu.Game.Online.Editor
 {
-    public class EditorRoomUser
+    [MessagePackObject]
+    [Serializable]
+    public class EditorRoomUser : IEquatable<EditorRoomUser>
     {
-        
+        [Key(0)]
+        public readonly int UserID;
+
+        [IgnoreMember]
+        public APIUser? User { get; set; }
+
+        [JsonConstructor]
+        public EditorRoomUser(int userId)
+        {
+            UserID = userId;
+        }
+
+        public bool Equals(EditorRoomUser? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return UserID == other.UserID;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((EditorRoomUser)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return UserID;
+        }
     }
 }
