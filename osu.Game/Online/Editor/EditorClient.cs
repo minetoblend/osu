6541c67940a671ffefbafbb8446cb49d88844e52
Marvin Schürz <minetoblend@gmail.com>
2023-08-22 18:03:37 +0200

    Add `IEditorServer`

--- osu.Game/Online/Editor/EditorClient.cs
+++ osu.Game/Online/Editor/EditorClient.cs
@@ -8,34 +8,13 @@ using osu.Framework.Graphics;
 
 namespace osu.Game.Online.Editor
 {
-    public abstract partial class EditorClient : Component, IEditorClient
+    public abstract partial class EditorClient : Component, IEditorClient, IEditorServer
     {
         /// <summary>
         /// Whether the <see cref="EditorClient"/> is currently connected.
         /// This is NOT thread safe and usage should be scheduled.
         /// </summary>
         public abstract IBindable<bool> IsConnected { get; }
-
-        /// <summary>
-        /// Creates an <see cref="EditorRoom"/> for a given beatmap.
-        /// </summary>
-        /// <param name="beatmap">The serialized beatmap containing the difficulty and all associated files.</param>
-        /// <returns>The joined <see cref="EditorRoom"/>.</returns>
-        protected abstract Task<EditorRoom> CreateAndJoinRoom(SerializedEditorBeatmap beatmap);
-
-        /// <summary>
-        /// Joins the <see cref="EditorRoom"/> with a given ID.
-        /// </summary>
-        /// <param name="roomId">The room ID.</param>
-        /// <returns>The joined <see cref="EditorRoom"/>.</returns>
-        protected abstract Task<EditorRoomJoinedResult> JoinRoom(long roomId);
-
-        /// <summary>
-        /// Submits a list of serialized commands to the server.
-        /// </summary>
-        /// <param name="commands">The commands to submit.</param>
-        /// <returns></returns>
-        protected abstract Task SubmitCommands(SerializedEditorCommands commands);
 
         Task IEditorClient.UserJoined(EditorRoomUser user)
         {
@@ -62,6 +41,36 @@ namespace osu.Game.Online.Editor
         }
 
         Task IEditorClient.CommandsSubmitted(EditorCommandEvent commands)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Task LeaveRoom()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Task<EditorRoom> CreateAndJoinRoom(SerializedEditorBeatmap beatmap)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Task<EditorRoomJoinedResult> JoinRoom(long roomId)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Task SubmitCommands(SerializedEditorCommands commands)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeState(EditorUserState state)
         {
             // TODO
             throw new NotImplementedException();
---
+++ osu.Game/Online/Editor/IEditorServer.cs
@@ -0,0 +1,38 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Threading.Tasks;
+
+namespace osu.Game.Online.Editor
+{
+    public interface IEditorServer
+    {
+        Task LeaveRoom();
+
+        /// <summary>
+        /// Creates an <see cref="EditorRoom"/> for a given beatmap.
+        /// </summary>
+        /// <param name="beatmap">The serialized beatmap containing the difficulty and all associated files.</param>
+        /// <returns>The joined <see cref="EditorRoom"/>.</returns>
+        Task<EditorRoom> CreateAndJoinRoom(SerializedEditorBeatmap beatmap);
+
+        /// <summary>
+        /// Joins the <see cref="EditorRoom"/> with a given ID.
+        /// </summary>
+        /// <param name="roomId">The room ID.</param>
+        /// <returns>The joined <see cref="EditorRoom"/>.</returns>
+        Task<EditorRoomJoinedResult> JoinRoom(long roomId);
+
+        /// <summary>
+        /// Submits a list of serialized commands to the server.
+        /// </summary>
+        /// <param name="commands">The commands to submit.</param>
+        Task SubmitCommands(SerializedEditorCommands commands);
+
+        /// <summary>
+        /// Change the local user state in the currently joined room.
+        /// </summary>
+        /// <param name="state"></param>
+        Task ChangeState(EditorUserState state);
+    }
+}
