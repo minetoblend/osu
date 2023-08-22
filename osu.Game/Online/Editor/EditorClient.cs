dc4b75e58bd95105ef1458607f208f97ad81bc81
Marvin Schürz <minetoblend@gmail.com>
2023-08-22 16:30:26 +0200

    Add basic hub connector configuration

--- osu.Game/Online/Editor/EditorClient.cs
+++ osu.Game/Online/Editor/EditorClient.cs
@@ -1,11 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 
 namespace osu.Game.Online.Editor
 {
     public abstract partial class EditorClient : Component, IEditorClient
     {
+        /// <summary>
+        /// Whether the <see cref="EditorClient"/> is currently connected.
+        /// This is NOT thread safe and usage should be scheduled.
+        /// </summary>
+        public abstract IBindable<bool> IsConnected { get; }
     }
 }
--- osu.Game/Online/Editor/OnlineEditorClient.cs
+++ osu.Game/Online/Editor/OnlineEditorClient.cs
@@ -1,15 +1,38 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Game.Online.API;
+
 namespace osu.Game.Online.Editor
 {
     public partial class OnlineEditorClient : EditorClient
     {
         private readonly string endpoint;
 
+        private IHubClientConnector? connector;
+
+        public override IBindable<bool> IsConnected { get; } = new BindableBool();
+
         public OnlineEditorClient(EndpointConfiguration endpoints)
         {
             endpoint = endpoints.EditorEndpointUrl;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(IAPIProvider api)
+        {
+            connector = api.GetHubConnector(nameof(OnlineEditorClient), endpoint);
+
+            if (connector != null)
+            {
+                connector.ConfigureConnection = connection =>
+                {
+                };
+
+                IsConnected.BindTo(connector.IsConnected);
+            }
         }
     }
 }
