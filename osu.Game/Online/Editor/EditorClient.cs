acc2cd59dd31d8dfe726c46c8657622b4d3a86e5
Marvin Schürz <minetoblend@gmail.com>
2023-08-22 16:35:14 +0200

    Add stubs for creating & joining rooms

--- osu.Game/Online/Editor/EditorClient.cs
+++ osu.Game/Online/Editor/EditorClient.cs
@@ -1,8 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Threading.Tasks;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics;
+using osu.Game.Beatmaps;
 
 namespace osu.Game.Online.Editor
 {
@@ -13,5 +15,20 @@ namespace osu.Game.Online.Editor
         /// This is NOT thread safe and usage should be scheduled.
         /// </summary>
         public abstract IBindable<bool> IsConnected { get; }
+
+        /// <summary>
+        /// Creates an <see cref="EditorRoom"/> for a given beatmap.
+        /// </summary>
+        /// <param name="beatmap">The beatmap to create the room for.</param>
+        /// <param name="beatmapSet">The beatmapset the beatmap belongs to.</param>
+        /// <returns>The joined <see cref="EditorRoom"/>.</returns>
+        protected abstract Task<EditorRoom> CreateAndJoinRoom(IBeatmapInfo beatmap, IBeatmapSetInfo beatmapSet);
+
+        /// <summary>
+        /// Joins the <see cref="EditorRoom"/> with a given ID.
+        /// </summary>
+        /// <param name="roomId">The room ID.</param>
+        /// <returns>The joined <see cref="EditorRoom"/>.</returns>
+        protected abstract Task<EditorRoom> JoinRoom(long roomId);
     }
 }
--- osu.Game/Online/Editor/OnlineEditorClient.cs
+++ osu.Game/Online/Editor/OnlineEditorClient.cs
@@ -1,8 +1,10 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Threading.Tasks;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Game.Beatmaps;
 using osu.Game.Online.API;
 
 namespace osu.Game.Online.Editor
@@ -33,6 +35,18 @@ namespace osu.Game.Online.Editor
 
                 IsConnected.BindTo(connector.IsConnected);
             }
+        }
+
+        protected override Task<EditorRoom> CreateAndJoinRoom(IBeatmapInfo beatmap, IBeatmapSetInfo beatmapSet)
+        {
+            //TODO
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task<EditorRoom> JoinRoom(long roomId)
+        {
+            //TODO
+            throw new System.NotImplementedException();
         }
     }
 }
