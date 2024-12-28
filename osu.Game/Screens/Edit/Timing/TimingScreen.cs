bd9d1d738833d4c2df569b2663a3d543dd20112d
Marvin Schürz <minetoblend@gmail.com>
2024-12-29 00:34:50 +0100

    Setup basic layout for timing screen

--- osu.Game/Screens/Edit/Timing/TimingScreen.cs
+++ osu.Game/Screens/Edit/Timing/TimingScreen.cs
@@ -2,64 +2,115 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Framework.Allocation;
-using osu.Framework.Bindables;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Game.Beatmaps.ControlPoints;
+using osu.Framework.Graphics.Effects;
+using osu.Framework.Graphics.Shapes;
+using osu.Game.Overlays;
 using osu.Game.Screens.Edit.Compose.Components.Timeline;
+using osuTK;
+using osuTK.Graphics;
 
 namespace osu.Game.Screens.Edit.Timing
 {
-    public partial class TimingScreen : EditorScreenWithTimeline
+    public partial class TimingScreen : EditorScreen
     {
-        [Cached]
-        public readonly Bindable<ControlPointGroup> SelectedGroup = new Bindable<ControlPointGroup>();
-
-        [Resolved]
-        private EditorClock? editorClock { get; set; }
-
         public TimingScreen()
             : base(EditorScreenMode.Timing)
         {
         }
 
-        protected override Drawable CreateMainContent() => new GridContainer
+        public Container HeaderContainer = null!;
+
+        private const float header_height = 20;
+        private const float header_width = 100;
+
+        [BackgroundDependencyLoader]
+        private void load(OverlayColourProvider colourProvider)
         {
-            RelativeSizeAxes = Axes.Both,
-            ColumnDimensions = new[]
+            AddRange(new Drawable[]
             {
-                new Dimension(),
-                new Dimension(GridSizeMode.Absolute, 350),
-            },
-            Content = new[]
-            {
-                new Drawable[]
+                new Container
                 {
-                    new ControlPointList(),
-                    new ControlPointSettings(),
+                    RelativeSizeAxes = Axes.Both,
+                    Padding = new MarginPadding { Left = header_width },
+                    Children = new Drawable[]
+                    {
+                        new Box
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Colour = colourProvider.Background2
+                        },
+                        new Timeline
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Children = new Drawable[]
+                            {
+                                new Container
+                                {
+                                    RelativeSizeAxes = Axes.X,
+                                    Height = header_height,
+                                    Masking = true,
+                                    Children = new Drawable[]
+                                    {
+                                        new Box
+                                        {
+                                            RelativeSizeAxes = Axes.Both,
+                                            Colour = colourProvider.Background1,
+                                        },
+                                        new TimelineTickDisplay
+                                        {
+                                            Y = 5
+                                        }
+                                    }
+                                },
+                                new Container
+                                {
+                                    RelativeSizeAxes = Axes.Both,
+                                    Padding = new MarginPadding { Top = header_height },
+                                    Children = new Drawable[]
+                                    {
+                                        new TimingScreenTickDisplay(),
+                                    }
+                                }
+                            }
+                        },
+                    },
                 },
-            }
-        };
-
-        protected override void LoadComplete()
-        {
-            base.LoadComplete();
-
-            if (editorClock != null)
-            {
-                // When entering the timing screen, let's choose the closest valid timing point.
-                // This will emulate the osu-stable behaviour where a metronome and timing information
-                // are presented on entering the screen.
-                var nearestTimingPoint = EditorBeatmap.ControlPointInfo.TimingPointAt(editorClock.CurrentTime);
-                SelectedGroup.Value = EditorBeatmap.ControlPointInfo.GroupAt(nearestTimingPoint.Time);
-            }
+                new Container
+                {
+                    RelativeSizeAxes = Axes.Y,
+                    Width = header_width,
+                    Masking = true,
+                    EdgeEffect = new EdgeEffectParameters
+                    {
+                        Type = EdgeEffectType.Shadow,
+                        Radius = 20,
+                        Colour = Colour4.Black.MultiplyAlpha(0.1f),
+                    },
+                    Children = new Drawable[]
+                    {
+                        new Box
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Colour = colourProvider.Background1
+                        },
+                        HeaderContainer = new Container
+                        {
+                            RelativeSizeAxes = Axes.Both
+                        }
+                    }
+                },
+            });
         }
 
-        protected override void ConfigureTimeline(TimelineArea timelineArea)
+        private partial class TimingScreenTickDisplay : TimelineTickDisplay
         {
-            base.ConfigureTimeline(timelineArea);
+            protected override Color4 GetColourFor(int divisor) => Color4.White;
 
-            timelineArea.Timeline.AlwaysShowControlPoints = true;
+            protected override float GetAlphaFor(int divisor) => divisor == 1 ? 0.15f : 0.075f;
+
+            protected override Vector2 GetSize(int indexInBar, int divisor) => base.GetSize(indexInBar, divisor) with { Y = 1 };
         }
     }
 }
