14af4567c441eb5ed8c2d9b35e7379f37badd117
marvin <minetoblend@gmail.com>
2025-12-02 18:23:17 +0100

    Bring discard screen animations in line with figma docs

--- osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/DiscardScreen.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/DiscardScreen.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
@@ -9,7 +10,10 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Effects;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Transforms;
 using osu.Framework.Input.Events;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
 using osu.Game.Graphics.Sprites;
 using osu.Game.Graphics.UserInterface;
 using osu.Game.Graphics.UserInterfaceV2;
@@ -22,13 +26,22 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay
     public partial class DiscardScreen(MultiplayerPlaylistItem[] hand) : RankedPlaySubScreen
     {
         private Container<Card> cardFlow = null!;
-
         private OsuButton discardButton = null!;
-        private OsuButton skipButton = null!;
+        private OsuSpriteText readyToGo = null!;
+        private OsuTextFlowContainer explainer = null!;
+
+        private readonly List<Card> cards = new List<Card>();
 
         [BackgroundDependencyLoader]
         private void load()
         {
+            cards.AddRange(hand.Select(item => new Card(item)
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                Action = cardClicked
+            }).ToArray());
+
             InternalChildren =
             [
                 cardFlow = new Container<Card>
@@ -37,12 +50,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay
                     Size = new Vector2(0.5f),
                     Anchor = Anchor.BottomCentre,
                     Origin = Anchor.BottomCentre,
-                    Children = hand.Select(item => new Card(item)
-                    {
-                        Origin = Anchor.Centre,
-                        Anchor = Anchor.Centre,
-                        Clicked = cardClicked
-                    }).ToArray(),
+                    Children = cards,
                 },
                 new FillFlowContainer
                 {
@@ -61,65 +69,210 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay
                             Origin = Anchor.Centre,
                             Size = new Vector2(100, 40),
                             Action = doDiscard,
-                            Enabled = { Value = false },
                             Name = "Discard"
-                        },
-                        skipButton = new RoundedButton
-                        {
-                            Text = "Skip",
-                            Anchor = Anchor.Centre,
-                            Origin = Anchor.Centre,
-                            Size = new Vector2(100, 40),
-                            Action = () =>
-                            {
-                                // TODO
-                            }
                         },
                     }
                 },
+                new FillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Width = 0.5f,
+                    Anchor = Anchor.TopCentre,
+                    Origin = Anchor.TopCentre,
+                    Direction = FillDirection.Vertical,
+                    Padding = new MarginPadding { Top = 50 },
+                    Spacing = new Vector2(10),
+                    Children =
+                    [
+                        new OsuSpriteText
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            Text = "Discarding Phase",
+                            Font = OsuFont.Style.Title
+                        },
+                        readyToGo = new OsuSpriteText
+                        {
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            Text = "You’re ready to go!",
+                            Font = OsuFont.Style.Subtitle,
+                            Colour = Color4Extensions.FromHex("#47B4EC"),
+                            Alpha = 0,
+                        },
+                        explainer = new OsuTextFlowContainer(s => s.Font = OsuFont.Style.Heading2)
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Anchor = Anchor.TopCentre,
+                            Origin = Anchor.TopCentre,
+                            TextAnchor = Anchor.TopCentre,
+                            Margin = new MarginPadding { Top = 50 },
+                            ParagraphSpacing = 1,
+                            Alpha = 0,
+                        }.With(d =>
+                        {
+                            d.AddParagraph("These are your Cards for this match!");
+                            d.AddParagraph("When it’s your pick, you can choose one card to go head-to-head with against your opponent!");
+                        })
+                    ]
+                }
             ];
 
-            const float spacing = -20;
-            float totalWidth = cardFlow.Children.Skip(1).Sum(card => card.Width + spacing) - spacing;
-
-            float x = -totalWidth / 2;
-
-            foreach (var card in cardFlow)
+            foreach (var (card, x) in layoutCards(cardFlow, -20))
             {
                 card.X = x;
-                card.Y = MathF.Pow(MathF.Abs(x / (totalWidth / 2)), 2) * 20;
-                card.Rotation = x * 0.03f;
-                x += card.Width + spacing;
+                card.Y = cardYOffsetAt(x);
+                card.Rotation = cardRotationAt(x);
             }
         }
 
+        private static float cardRotationAt(float x) => x * 0.03f;
+        private static float cardYOffsetAt(float x) => MathF.Pow(MathF.Abs(x / 250), 2) * 20;
+
         private void cardClicked(Card target)
         {
-            if (didDiscard)
-                return;
-
             target.Selected = !target.Selected;
 
-            discardButton.Enabled.Value = cardFlow.Any(it => it.Selected);
+            discardButton.Text = cardFlow.Any(it => it.Selected) ? "Discard" : "Don't Discard";
         }
-
-        private bool didDiscard;
 
         private void doDiscard()
         {
-            didDiscard = true;
+            discardButton.Hide();
 
-            var cards = cardFlow.Where(it => it.Selected).ToList();
+            foreach (var card in cards)
+                card.Action = null;
 
-            const float spacing = 20;
-            float totalWidth = cards.Skip(1).Sum(it => it.Width + spacing) - spacing;
+            double delay = 0;
+
+            playDiscardAnimation(ref delay);
+            flyInNewCards(ref delay /*TODO: pass in newCards parameter */);
+            presentRemainingCards(ref delay);
+        }
+
+        #region Animations
+
+        private void playDiscardAnimation(ref double delay)
+        {
+            cards.RemoveAll(card => card.Selected);
+
+            var selectedCards = cardFlow.Where(it => it.Selected).ToList();
+
+            if (selectedCards.Count == 0)
+                return;
+
+            foreach (var (card, x) in layoutCards(selectedCards, 20))
+            {
+                card.Delay(delay)
+                    .MoveTo(new Vector2(x, -200), 400, Easing.OutExpo)
+                    .RotateTo(0, 400, Easing.OutExpo)
+                    .Then(400)
+                    .ScaleTo(0, 350)
+                    .FadeOut(200)
+                    .Expire();
+
+                delay += 50;
+            }
+
+            foreach (var (card, x) in layoutCards(cards, -20))
+            {
+                card
+                    .Delay(delay)
+                    .MoveToX(x, 400, Easing.OutExpo)
+                    .MoveToY(cardYOffsetAt(x), 400, Easing.OutExpo)
+                    .RotateTo(cardRotationAt(x), 400, Easing.OutExpo);
+
+                delay += 50;
+            }
+
+            delay += 1500;
+        }
+
+        private void flyInNewCards(ref double delay)
+        {
+            if (cards.Count == 5)
+                return;
+
+            const double stagger = 100;
+
+            var newCards = new List<Card>();
+
+            while (cards.Count < 5)
+            {
+                var newCard = new Card(new MultiplayerPlaylistItem())
+                {
+                    Origin = Anchor.Centre,
+                    Anchor = Anchor.Centre,
+                    Alpha = 0,
+                    X = DrawWidth * 0.75f
+                };
+
+                cards.Add(newCard);
+                cardFlow.Add(newCard);
+                newCards.Add(newCard);
+            }
+
+            var cardLayout = layoutCards(cards, -20)
+                             .Select(x => new { x.card, x.x, isNew = newCards.Contains(x.card) })
+                             .ToList();
+
+            foreach (var entry in cardLayout.Where(it => !it.isNew))
+            {
+                entry.card
+                     .Delay(delay)
+                     .MoveToX(entry.x, 1000, Easing.OutExpo)
+                     .MoveToY(cardYOffsetAt(entry.x), 1000, Easing.OutExpo)
+                     .RotateTo(cardRotationAt(entry.x), 1000, Easing.OutExpo);
+
+                delay += stagger / 2;
+            }
+
+            foreach (var entry in cardLayout.Where(it => it.isNew))
+            {
+                entry.card
+                     .Delay(delay)
+                     .FadeIn()
+                     .MoveToX(entry.x, 600, Easing.OutExpo)
+                     .MoveToY(cardYOffsetAt(entry.x), 600, Easing.OutExpo)
+                     .RotateTo(cardRotationAt(entry.x), 600, Easing.OutExpo);
+
+                delay += stagger;
+            }
+
+            delay += 1000;
+        }
+
+        private void presentRemainingCards(ref double delay)
+        {
+            const double stagger = 20;
+
+            readyToGo.Delay(delay).FadeIn(50);
+            explainer.Delay(delay).FadeIn(50);
+
+            foreach (var (card, x) in layoutCards(cards, 20))
+            {
+                card
+                    .Delay(delay)
+                    .MoveTo(new Vector2(x, -30), 400, new CubicBezierEasingFunction(0.3, -0.05, 0.0, 1.0))
+                    .RotateTo(0, 400, new CubicBezierEasingFunction(0.3, -0.05, 0.0, 1.0));
+
+                delay += stagger;
+            }
+        }
+
+        #endregion
+
+        private IEnumerable<(Card card, float x)> layoutCards(IEnumerable<Card> cards, float spacing)
+        {
+            float totalWidth = cards.Sum(card => card.Width + spacing) - spacing;
+
             float x = -totalWidth / 2;
 
             foreach (var card in cards)
             {
-                card.MoveTo(new Vector2(x, -200), 400, Easing.OutExpo)
-                    .RotateTo(0, 400, Easing.OutExpo);
-                x += card.Width + spacing;
+                yield return (card, x + card.LayoutSize.X / 2);
+
+                x += card.LayoutSize.X + spacing;
             }
         }
 
@@ -180,7 +333,7 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay
                 };
             }
 
-            public required Action<Card> Clicked;
+            public Action<Card>? Action;
 
             private bool selected;
 
@@ -194,10 +347,26 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay
                 }
             }
 
+            public override bool HandlePositionalInput => Action != null;
+
+            public override bool Contains(Vector2 screenSpacePos) => content.Contains(screenSpacePos);
+
             protected override bool OnClick(ClickEvent e)
             {
-                Clicked(this);
+                Action?.Invoke(this);
                 return true;
+            }
+
+            protected override bool OnHover(HoverEvent e)
+            {
+                content.ScaleTo(1.1f, 300, Easing.OutElasticQuarter);
+
+                return true;
+            }
+
+            protected override void OnHoverLost(HoverLostEvent e)
+            {
+                content.ScaleTo(1f, 300, Easing.OutElasticQuarter);
             }
         }
     }
