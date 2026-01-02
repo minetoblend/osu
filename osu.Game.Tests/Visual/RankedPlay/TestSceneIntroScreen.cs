516a0d2ad97cfccf8a53df61fe88d78e816f86ac
Marvin <minetoblend@gmail.com>
2026-01-02 16:47:59 +0100

    Initial implementation for intro screen

--- osu.Game.Tests/Visual/RankedPlay/TestSceneIntroScreen.cs
+++ osu.Game.Tests/Visual/RankedPlay/TestSceneIntroScreen.cs
@@ -2,8 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using osu.Game.Online.API.Requests.Responses;
-using osu.Game.Online.Rooms;
-using osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay;
+using osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Intro;
 using osu.Game.Tests.Visual.Matchmaking;
 
 namespace osu.Game.Tests.Visual.RankedPlay
@@ -14,12 +13,25 @@ namespace osu.Game.Tests.Visual.RankedPlay
         {
             base.SetUpSteps();
 
-            AddStep("join room", () => JoinRoom(CreateDefaultRoom(MatchType.RankedPlay)));
-            WaitForJoined();
+            IntroScreen introScreen = null!;
 
-            AddStep("join other user", () => MultiplayerClient.AddUser(new APIUser { Id = 2 }));
+            AddStep("Add screen", () => Child = introScreen = new IntroScreen());
 
-            AddStep("Add screen", () => Child = new IntroScreen());
+            AddStep("play animation", () => introScreen.PlayIntroSequence(
+                new UserWithRating(new APIUser
+                {
+                    Id = 2,
+                    Username = "User 1",
+                    CoverUrl = "https://assets.ppy.sh/user-profile-covers/13845312/53e4eda7ad3ce41f0990c041179d8ab5d553fef988835f346a8d8da0482506ec.png"
+                }, 1234),
+                new UserWithRating(new APIUser
+                {
+                    Id = 3,
+                    Username = "User 2",
+                    CoverUrl = "https://assets.ppy.sh/user-profile-covers/14102976/10144df2f1c6fb2101726e0f89087a6061bc75755d88e59a9faf2c84034f2c71.jpeg"
+                }, 1234),
+                6.3f
+            ));
         }
     }
 }
