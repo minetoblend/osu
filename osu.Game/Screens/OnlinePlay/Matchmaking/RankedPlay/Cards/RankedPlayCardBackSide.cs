7137987b1aa22ad3830a42a01e39853300b57ba2
Marvin <minetoblend@gmail.com>
2026-01-29 20:36:15 +0100

    add assert for cardback texture aspect-ratio

--- osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Cards/RankedPlayCardBackSide.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Cards/RankedPlayCardBackSide.cs
@@ -1,12 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Diagnostics;
 using osu.Framework.Allocation;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
+using osu.Framework.Utils;
 
 namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Cards
 {
@@ -25,10 +27,14 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Cards
             BorderThickness = 3;
             BorderColour = Color4Extensions.FromHex("9F2C64");
 
+            var texture = textures.Get("Online/RankedPlay/cardback");
+
+            Debug.Assert(Precision.AlmostEquals((float)texture.Width / texture.Height, DrawWidth / DrawHeight));
+
             InternalChild = new Sprite
             {
                 RelativeSizeAxes = Axes.Both,
-                Texture = textures.Get("Online/RankedPlay/cardback"),
+                Texture = texture,
             };
         }
     }
