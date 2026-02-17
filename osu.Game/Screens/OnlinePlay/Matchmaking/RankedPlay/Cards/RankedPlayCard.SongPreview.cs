b5b1a7e5cc3b79c03270b33445018df478bcefcb
Marvin <minetoblend@gmail.com>
2026-02-17 14:19:06 +0100

    Implement BeatSyncProvider for SongPreviewContainer

--- osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Cards/RankedPlayCard.SongPreview.cs
+++ osu.Game/Screens/OnlinePlay/Matchmaking/RankedPlay/Cards/RankedPlayCard.SongPreview.cs
@@ -3,33 +3,49 @@
 
 using System.Diagnostics;
 using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Audio.Track;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Transforms;
 using osu.Framework.Input.Events;
+using osu.Framework.Timing;
 using osu.Game.Audio;
+using osu.Game.Beatmaps;
+using osu.Game.Beatmaps.ControlPoints;
+using osu.Game.Graphics.Containers;
 using osu.Game.Online.API.Requests.Responses;
 
 namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Cards
 {
     public partial class RankedPlayCard
     {
-        public partial class SongPreviewContainer : Container
+        public partial class SongPreviewContainer : Container, IBeatSyncProvider
         {
+            [Resolved]
+            private PreviewTrackManager previewTrackManager { get; set; } = null!;
+
             protected override Container<Drawable> Content { get; }
 
             public SongPreviewContainer()
             {
                 InternalChildren =
                 [
-                    Content = new Container
+                    new PulsingContainer
                     {
                         RelativeSizeAxes = Axes.Both,
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Children =
+                        [
+                            Content = new Container
+                            {
+                                RelativeSizeAxes = Axes.Both,
+                            },
+                        ]
                     },
                 ];
             }
-
-            [Resolved]
-            private PreviewTrackManager previewTrackManager { get; set; } = null!;
 
             private PreviewTrack? previewTrack;
 
@@ -40,6 +56,8 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Cards
                 LoadComponentAsync(previewTrack = previewTrackManager.Get(beatmap.BeatmapSet!), track =>
                 {
                     AddInternal(track);
+
+                    setupBeatSyncProvider(track, beatmap);
 
                     if (IsHovered)
                         startPreviewIfAvailable();
@@ -54,6 +72,49 @@ namespace osu.Game.Screens.OnlinePlay.Matchmaking.RankedPlay.Cards
             }
 
             private void startPreviewIfAvailable() => previewTrack?.Start();
+
+            private partial class PulsingContainer : BeatSyncedContainer
+            {
+                protected override void OnNewBeat(int beatIndex, TimingControlPoint timingPoint, EffectControlPoint effectPoint, ChannelAmplitudes amplitudes)
+                {
+                    if (!IsBeatSyncedWithTrack)
+                        return;
+
+                    this.ScaleTo(1.02f, timingPoint.BeatLength * 0.2f)
+                        .Then()
+                        .ScaleTo(1f, timingPoint.BeatLength, new CubicBezierEasingFunction(easeIn: 0.1f, easeOut: 1f));
+                }
+            }
+
+            #region IBeatSyncProvider implementation
+
+            private readonly PreviewTrackClock beatSyncClock = new PreviewTrackClock();
+            private readonly ControlPointInfo controlPoints = new ControlPointInfo();
+
+            ChannelAmplitudes IHasAmplitudes.CurrentAmplitudes => ChannelAmplitudes.Empty;
+            ControlPointInfo IBeatSyncProvider.ControlPoints => controlPoints;
+            IClock IBeatSyncProvider.Clock => beatSyncClock;
+
+            private void setupBeatSyncProvider(PreviewTrack track, APIBeatmap beatmap)
+            {
+                beatSyncClock.Track = track;
+
+                controlPoints.Add(0, new TimingControlPoint
+                {
+                    BeatLength = beatmap.BPM > 0 ? 60_000 / beatmap.BPM : TimingControlPoint.DEFAULT_BEAT_LENGTH
+                });
+            }
+
+            private class PreviewTrackClock : IClock
+            {
+                public PreviewTrack? Track { get; set; }
+
+                public double CurrentTime => Track?.CurrentTime ?? 0;
+                public double Rate => 1;
+                public bool IsRunning => Track?.IsRunning ?? false;
+            }
+
+            #endregion
         }
     }
 }
