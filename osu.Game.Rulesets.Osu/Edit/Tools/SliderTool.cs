bf07872be0afca65d546797b2670f1088cfd52a2
marvin <minetoblend@gmail.com>
2026-05-28 19:05:19 +0200

    Use distance snap algorithm from lazer

--- osu.Game.Rulesets.Osu/Edit/Tools/SliderTool.cs
+++ osu.Game.Rulesets.Osu/Edit/Tools/SliderTool.cs
@@ -1,14 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Input.Events;
 using osu.Framework.Utils;
+using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Objects;
 using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Osu.Objects;
 using osu.Game.Rulesets.UI;
+using osu.Game.Screens.Edit;
 using osuTK;
 using osuTK.Input;
 
@@ -23,6 +26,9 @@ namespace osu.Game.Rulesets.Osu.Edit.Tools
         }
 
         private readonly List<PathControlPoint> path = [new PathControlPoint(Vector2.Zero, PathType.BEZIER)];
+
+        [Resolved]
+        private BindableBeatDivisor beatDivisor { get; set; } = null!;
 
         protected override void UpdateTimeAndPosition(Vector2 position, double time)
         {
@@ -47,7 +53,7 @@ namespace osu.Game.Rulesets.Osu.Edit.Tools
                     if (Vector2.Distance(path[^1].Position, position) > 3)
                         HitObject.Path.ControlPoints.Add(new PathControlPoint(position));
 
-                    // todo: updateLength
+                    resnapSliderLength();
 
                     if (Precision.DefinitelyBigger(HitObject.SpanDuration, 0))
                         HitObject.RepeatCount = int.Clamp((int)((time - HitObject.StartTime) / HitObject.SpanDuration), 0, 100);
@@ -95,6 +101,27 @@ namespace osu.Game.Rulesets.Osu.Edit.Tools
             }
 
             return true;
+        }
+
+        [Resolved]
+        private IBeatSnapProvider beatSnapProvider { get; set; } = null!;
+
+        private void resnapSliderLength()
+        {
+            double snapReferenceTime = HitObject.StartTime;
+
+            double actualDuration = snapReferenceTime + HitObject.Path.CalculatedDistance / HitObject.Velocity;
+
+            double snappedTime = beatSnapProvider.SnapTime(actualDuration, snapReferenceTime);
+
+            double beatLength = beatSnapProvider.GetBeatLengthAtTime(snapReferenceTime);
+
+            // we don't want to exceed the actual duration and snap to a point in the future.
+            // as we are snapping to beat length via SnapTime (which will round-to-nearest), check for snapping in the forward direction and reverse it.
+            if (snappedTime > actualDuration + 1)
+                snappedTime -= beatLength;
+
+            HitObject.Path.ExpectedDistance.Value = Math.Min((snappedTime - snapReferenceTime) * HitObject.Velocity, HitObject.Path.CalculatedDistance);
         }
     }
 }
