54e0c5875c92d5cc477a20f7e4d5bb064eb6326e
marvin <m.schuerz@hautzy.com>
2026-09-19 13:52:26 +0200

    Adjust motion blur based on actual refresh rate

--- osu.Game.Rulesets.Osu/UI/Cursor/CursorMotionBlur.cs
+++ osu.Game.Rulesets.Osu/UI/Cursor/CursorMotionBlur.cs
@@ -1,10 +1,14 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
 using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
+using osu.Framework.Platform;
+using osu.Framework.Timing;
 using osu.Game.Rulesets.Osu.Skinning;
 using osu.Game.Skinning;
 using osuTK.Graphics;
@@ -13,17 +17,34 @@ namespace osu.Game.Rulesets.Osu.UI.Cursor
 {
     public partial class CursorMotionBlur : CursorTrail
     {
-        /// <summary>
-        /// Adjust this to ur monitor refresh rate
-        /// </summary>
-        public double ShutterDuration { get; set; } = 1000 / 240.0;
+        public double ShutterDuration
+        {
+            get
+            {
+                double fps = drawClock?.FramesPerSecond ?? 0;
+                float refreshRate = displayMode?.Value.RefreshRate ?? 0;
 
-        public float Opacity => 0.25f;
+                if (refreshRate > 0)
+                {
+                    if (fps > 0)
+                        fps = Math.Min(fps, refreshRate);
+                    else
+                        fps = refreshRate;
+                }
+
+                return fps > 0 ? 1000 / fps : 1000 / 60.0;
+            }
+        }
+
+        private IFrameBasedClock? drawClock;
+        private IBindable<DisplayMode>? displayMode;
+
+        public float Opacity => 0.2f;
 
         public float Spacing { get; set; } = 0.04f;
 
         protected override double FadeDuration => ShutterDuration;
-        protected override float FadeExponent => 0.01f;
+        protected override float FadeExponent => 0.05f;
 
         protected override bool InterpolateMovements => true;
         protected override float IntervalMultiplier => Spacing * 2.5f;
@@ -38,8 +59,11 @@ namespace osu.Game.Rulesets.Osu.UI.Cursor
         }
 
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(GameHost host)
         {
+            drawClock = host.DrawThread.Clock;
+            displayMode = host.Window?.CurrentDisplayMode.GetBoundCopy();
+
             skin.SourceChanged += updateTexture;
             updateTexture();
         }
